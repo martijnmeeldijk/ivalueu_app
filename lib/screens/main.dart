3e1246a83f41b9ed6f04d492c43c5109e973f3b8
Epic Martijn <martijn.meeldijk@student.ucll.be>
2020-10-09 14:43:25 +0100

    Initial commit

---
+++ lib/screens/main.dart
@@ -0,0 +1,40 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CustomTextContainer(),
+              CustomTextContainer(),
+              CustomTextContainer(),
+            ],
+
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextContainer extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Text('peieieiie');
+  }
+}
